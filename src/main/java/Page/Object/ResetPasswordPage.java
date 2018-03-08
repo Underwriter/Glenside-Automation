9596999c61fd173c7d6fdaa0adf988c575a1b8d9
Underwriter <pglomozda@gmail.com>
2018-03-08 23:30:38 +0200

    refactoring

--- src/main/java/Page/Object/ResetPasswordPage.java
+++ src/main/java/Page/Object/ResetPasswordPage.java
@@ -12,19 +12,21 @@ public class ResetPasswordPage {
     }
 
     private By pageName = By.xpath("//span[@class='reset-form__title']");
-    private By logo = By.xpath("//div[@class='header__logo reset-password__logo']/a/img/@src");
+   /* private By logo = By.xpath("//div[@class='header__logo reset-password__logo']/a/img/@src");
     private By logInButton = By.xpath("//button[@class='reset-form__log-in-btn']");
-    private By resetPasswordButton = By.xpath("//button[@class='reset-form__reset-password-btn']");
+    private By resetPasswordButton = By.xpath("//button[@class='reset-form__reset-password-btn']");*/
     private By emailField = By.xpath("//input[@id='id_email']");
 
+/*
     private ResetPasswordPage typeEmail(String email) {
         driver.findElement(emailField).sendKeys(email);
         return this;
     }
+*/
 
-    private String getPageName() {
+   /* private String getPageName() {
         return driver.findElement(pageName).getText();
     }
-
+*/
 }
 
